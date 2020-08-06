ffed141bda8e9c215458d17f09ba423060e9cd27
Lucas Guilherme <lukeg299@gmail.com>
2020-08-06 20:35:24 -0300

    Initial commit

---
+++ src/test/java/io/frigo/lucas/covidinfotracker/CovidInfoTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.frigo.lucas.covidinfotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidInfoTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
